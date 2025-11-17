f3b55c51753ac53b74b370c3bbce2650cedb49c1
Mikey Jay <mikeyjay39@gmail.com>
2025-11-16 20:06:28 -0800

    Add working ollama docker config
    TODO: Add integration test and replace ollama call in unit test with a
    mock

--- backend/src/infrastructure/ollama_document_summarizer_adapter.rs
+++ backend/src/infrastructure/ollama_document_summarizer_adapter.rs
@@ -5,7 +5,7 @@ use ollama_rs::{Ollama, generation::completion::request::GenerationRequest};
 use crate::domain::document_summarizer::DocumentSummarizer;
 
 // TODO: How does Rust do string constants?
-const MODEL_NAME: &str = "llama2-latest";
+const MODEL_NAME: &str = "llama2";
 const SUMMARY_LENGTH: usize = 200;
 
 pub struct OllamaDocumentSummarizerAdapter {
@@ -31,12 +31,38 @@ impl Default for OllamaDocumentSummarizerAdapter {
 impl DocumentSummarizer for OllamaDocumentSummarizerAdapter {
     async fn summarize(&self, text: &str) -> Result<String, Box<dyn Error>> {
         let prompt = format!(
-            "Summarize the following text in less than {} characters:\n\n{}",
+            "Summarize the following text in a single sentence that is less than {} characters:\n\n{}",
             SUMMARY_LENGTH, text
         );
 
         let request = GenerationRequest::new(MODEL_NAME.to_string(), prompt);
         let response = self.ollama_client.generate(request).await?;
         Ok(response.response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::document_summarizer::DocumentSummarizer;
+    use tokio;
+
+    // TODO: Mock the Ollama client for testing and add integration tests for this
+    #[tokio::test]
+    async fn test_summarize() {
+        let summarizer = OllamaDocumentSummarizerAdapter::new();
+        let text = "Rust is a systems programming language that runs blazingly fast, prevents segfaults, and guarantees thread safety. It is designed to be a safe, concurrent, and practical language that supports functional and imperative-procedural paradigms. Rust is syntactically similar to C++, but it provides better memory safety while maintaining performance.";
+        let summary_result = summarizer.summarize(text).await;
+        match summary_result {
+            Ok(summary) => {
+                println!("Summary of text: {}", summary);
+                let summary_length = summary.chars().count();
+                println!("Summary length: {}", summary_length);
+                assert!(summary_length < SUMMARY_LENGTH);
+            }
+            Err(e) => {
+                panic!("Summarization failed with error: {}", e);
+            }
+        }
     }
 }
