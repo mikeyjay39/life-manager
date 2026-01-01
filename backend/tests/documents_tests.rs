d998e3b839d9aede89d4caebbfffcaabf27e03ec
Michael Jeszenka <mikeyjay39@gmail.com>
2026-01-01 10:55:58 -0800

    fix: fix integration test in ci

--- backend/tests/documents_tests.rs
+++ backend/tests/documents_tests.rs
@@ -3,7 +3,6 @@ mod common;
 use std::fs;
 
 use axum_test::TestServer;
-use common::setup::run_test;
 use life_manager::infrastructure::{
     document_dto::DocumentDto, document_handler::CreateDocumentCommand,
 };
@@ -11,9 +10,7 @@ use reqwest::multipart::{Form, Part};
 use serial_test::serial;
 use tracing_test::traced_test;
 
-use crate::common::setup::{
-    IntegrationTestContainer, run_test_with_all_containers, run_test_with_test_profile,
-};
+use crate::common::setup::{run_test_with_all_containers, run_test_with_test_profile};
 use reqwest::ClientBuilder;
 use std::time::Duration;
 
@@ -189,61 +186,59 @@ async fn create_and_get_document() {
 #[serial]
 #[traced_test]
 async fn create_and_get_document_no_file() {
-    run_test(
-        |_container: &IntegrationTestContainer, server: TestServer| async move {
-            // Seed 1 document into the database
-            let payload = CreateDocumentCommand {
-                id: 2,
-                title: String::from("Integration Test Document"),
-                content: String::from("This is a test content."),
-            };
-            // Make REST API call to create a document
-            let json_string = serde_json::to_string(&payload).unwrap();
+    run_test_with_test_profile(|server: TestServer| async move {
+        // Seed 1 document into the database
+        let payload = CreateDocumentCommand {
+            id: 2,
+            title: String::from("Integration Test Document"),
+            content: String::from("This is a test content."),
+        };
+        // Make REST API call to create a document
+        let json_string = serde_json::to_string(&payload).unwrap();
 
-            let multipart_body = format!(
-                "--boundary\r\n\
+        let multipart_body = format!(
+            "--boundary\r\n\
         Content-Disposition: form-data; name=\"json\"\r\n\
         Content-Type: application/json\r\n\r\n\
         {}\r\n\
         --boundary--",
-                json_string
-            );
+            json_string
+        );
 
-            let url_result = server
-                .server_url("/documents")
-                .expect("Failed to get server URL");
-            let url = url_result.as_str();
-            tracing::info!("URL: {}", url);
-            let res = reqwest::Client::new()
-                .post(url)
-                .body(multipart_body)
-                .header("Content-Type", "multipart/form-data; boundary=boundary")
-                .send()
-                .await
-                .expect("Failed to send request");
-            tracing::info!("Response: {:?}", res);
-            assert!(res.status().is_success());
+        let url_result = server
+            .server_url("/documents")
+            .expect("Failed to get server URL");
+        let url = url_result.as_str();
+        tracing::info!("URL: {}", url);
+        let res = reqwest::Client::new()
+            .post(url)
+            .body(multipart_body)
+            .header("Content-Type", "multipart/form-data; boundary=boundary")
+            .send()
+            .await
+            .expect("Failed to send request");
+        tracing::info!("Response: {:?}", res);
+        assert!(res.status().is_success());
 
-            let response_document = res.json::<DocumentDto>().await.unwrap();
+        let response_document = res.json::<DocumentDto>().await.unwrap();
 
-            // Verify the document was created in the database
+        // Verify the document was created in the database
 
-            let get_request_url_result = server
-                .server_url(&format!("/documents/{}", &response_document.id))
-                .expect("Failed to get server URL");
-            let get_request_url = get_request_url_result.as_str();
-            tracing::info!("Get Request URL: {}", get_request_url);
-            let get_response = reqwest::Client::new()
-                .get(get_request_url)
-                .send()
-                .await
-                .expect("Failed to send request");
-            tracing::info!("Get Response: {:?}", get_response);
-            assert!(get_response.status().is_success());
-            let document: DocumentDto = get_response.json().await.unwrap();
-            assert_eq!(document.title, payload.title);
-            assert_eq!(document.content, payload.content);
-        },
-    )
+        let get_request_url_result = server
+            .server_url(&format!("/documents/{}", &response_document.id))
+            .expect("Failed to get server URL");
+        let get_request_url = get_request_url_result.as_str();
+        tracing::info!("Get Request URL: {}", get_request_url);
+        let get_response = reqwest::Client::new()
+            .get(get_request_url)
+            .send()
+            .await
+            .expect("Failed to send request");
+        tracing::info!("Get Response: {:?}", get_response);
+        assert!(get_response.status().is_success());
+        let document: DocumentDto = get_response.json().await.unwrap();
+        assert_eq!(document.title, payload.title);
+        assert_eq!(document.content, payload.content);
+    })
     .await;
 }
