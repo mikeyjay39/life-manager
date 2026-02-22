3144006bef5bc216b092b985d88b8a24cc57166a
Mikey Jay <mikeyjay39@gmail.com>
2026-02-22 04:58:35 +0000

    Remove commented code

--- backend/.container/run.sh
+++ backend/.container/run.sh
@@ -33,37 +33,3 @@ docker run -it --rm \
   rust-nvim \
   zsh
 
-# docker run -it \
-#   -v "$TEMP_GPG_DIR:/home/$USERNAME/.gnupg" \
-#   --network host \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#   -w /home/$USERNAME/life-manager \
-#   rust-nvim \
-#   zsh
-
-# Cleanup happens automatically via trap
-# docker run -it --rm \
-#   -v "$PWD":/workspace \
-#   \
-#   -v "$HOME/.config/nvim:/root/.config/nvim:ro" \
-#   -v "$HOME/.config/ghostty:/root/.config/ghostty:ro" \
-#   \
-#   -v "$HOME/.local/share/nvim:/root/.local/share/nvim" \
-#   -v "$HOME/.local/state/nvim:/root/.local/state/nvim" \
-#   -v "$HOME/.cache/nvim:/root/.cache/nvim" \
-#   \
-#   -v cargo-registry:/root/.cargo/registry \
-#   -v cargo-git:/root/.cargo/git \
-#   -v rust-target:/workspace/target \
-#   \
-#   --network host \
-#   -v /var/run/docker.sock:/var/run/docker.sock \
-#   \
-#   -e WAYLAND_DISPLAY \
-#   -e XDG_RUNTIME_DIR \
-#   -v "$XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR" \
-#   \
-#   -w /workspace \
-#   \
-#   rust-nvim \
-#   sh
